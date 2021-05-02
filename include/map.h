6152062ad9a20e2693aa4c7d168ca4d15c5cdf0f
Liang Wang <bill79231725@gmail.com>
2021-05-02 19:08:21 -0400

    implement ceiling and floor functions

--- include/map.h
+++ include/map.h
@@ -33,6 +33,8 @@ class map {
         constexpr auto& key() noexcept { return key_val_.first; }
 
         constexpr auto& value() noexcept { return key_val_.second; }
+
+        constexpr auto& key_val() noexcept { return key_val_; }
     };
 
    public:
@@ -42,7 +44,7 @@ class map {
 
     map() noexcept = default;
 
-    [[nodiscard]] constexpr V const& at(K const& key) const {
+    [[nodiscard]] constexpr value_type const& at(K const& key) const {
         auto* value = find(root, key);
         if (!value) {
             throw std::out_of_range{"such key does not exist!"};
@@ -51,7 +53,7 @@ class map {
         }
     }
 
-    [[nodiscard]] constexpr V& at(K const& key) {
+    [[nodiscard]] constexpr value_type& at(K const& key) {
         auto* target = find(root, key);
         if (!target) {
             throw std::out_of_range{"such key does not exist!"};
@@ -66,11 +68,12 @@ class map {
             return;
         } else {
             // we walk through every unique pointer until it points to null, meaning we have reached a leaf node
+            // the `iter` is is a pointer to a std::unique_ptr, hence double de-referencing is required to get the underlying element
             auto* iter = &root;
             while (*iter) {
-                auto comp = key_val.first <=> (*iter)->key_val_.first;
+                auto comp = key_val.first <=> (*iter)->key();
                 if (comp == std::strong_ordering::equal) {
-                    (*iter)->key_val_ = std::move(key_val);
+                    (*iter)->key_val() = std::move(key_val);
                 } else if (comp == std::strong_ordering::less) {
                     iter = &(*iter)->left_;
                 } else {
@@ -81,14 +84,19 @@ class map {
         }
     }
 
+    // TODO: implement this!
     void remove(K const& key) {}
 
     [[nodiscard]] constexpr std::size_t size() const { return size(root); }
 
-    constexpr key_type const& min() const noexcept { return static_cast<key_type const&>(*min_impl()); }
-    constexpr key_type& min() noexcept { return *min_impl(); }
+    constexpr node_type const& min() const noexcept { return static_cast<key_type const&>(*min_impl()); }
+    constexpr node_type& min() noexcept { return *min_impl(); }
 
-    constexpr key_type& floor(key_type const& key) const noexcept {}
+    constexpr node_type const& floor(key_type const& key) const { return static_cast<node_type const&>(*floor_impl(root.get(), key)); }
+    constexpr node_type& floor(key_type const& key) { return *floor_impl(root.get(), key); }
+
+    constexpr node_type const& ceiling(key_type const& key) const { return static_cast<node_type const&>(*ceiling_impl(root.get(), key)); }
+    constexpr node_type& ceiling(key_type const& key) { return *ceiling_impl(root.get(), key); }
 
    private:
     template <typename T, typename U>
@@ -102,48 +110,73 @@ class map {
     constexpr node_type* find(node_type* node, K const& key) const noexcept {
         auto* node_iter = node;
         while (node_iter) {
-            if (node_iter->key == key) {
+            if (node_iter->key() == key) {
                 return node_iter;
             }
 
-            if (key < node_iter->key) {
+            if (key < node_iter->key()) {
                 node_iter = node_iter->left_.get();
             }
-            if (key > node_iter->key) {
+            if (key > node_iter->key()) {
                 node_iter = node_iter->right_.get();
             }
         }
         return node_iter;
     }
 
-    constexpr key_type* min_impl() noexcept {
+    constexpr node_type* min_impl() noexcept {
         auto* iter = root.get();
         if (iter) {
             while (iter) {
                 iter = iter->left_.get();
             }
-            return iter->get().key_val_.first;
+            return iter->get();
         }
         return nullptr;
     }
 
-    constexpr key_type* floor_impl(node_type const* node, key_type const& key) noexcept {
+    constexpr node_type* floor_impl(node_type const* node, key_type const& key) noexcept {
         if (!node) {
             return nullptr;
         }
 
-        auto comp = key <=> node->key();
+        auto const comp = key <=> node->key();
 
         if (comp == std::strong_ordering::equal) {
-            return node->key();
+            return node;
         } else if (comp == std::strong_ordering::less) {
             return floor_impl(node->left_.get(), key);
         } else {
-            auto* smallest_in_right_tree = floor(node->right_.get(), key);
-            if (smallest_in_right_tree) {
-                return smallest_in_right_tree;
+            // there might be an element in the right subtree that is greater than the key of `node` but smaller than `key`, we do not know if such element
+            // exists until we search through all of them
+            if (auto* smaller_in_right_subtree = floor_impl(node->right_.get(), key)) {
+                // if there is an element that is bigger than `node` but smaller than `key`, return it
+                return smaller_in_right_subtree;
             } else {
-                return node->key();
+                // otherwise, that means all elements in the right subtrees are bigger, `node` is the floor of key
+                return node;
+            }
+        }
+    }
+
+    consteval node_type* ceiling_impl(node_type const* node, key_type const& key) {
+        if (!node) {
+            return nullptr;
+        }
+
+        auto const comp = key <=> node->key();
+        if (comp == std::strong_ordering::equal) {
+            return node;
+        } else if (comp == std::strong_ordering::greater) {
+            return ceiling_impl(node->right_.get(), key);
+        } else {
+            // if `key` is smaller than the key of node`, then there might be an element that is even smaller than node but bigger than key
+            if (auto* larger_element = ceiling_impl(node->left_.get(), key)) {
+                // if there is an element that is smaller than `node` but bigger than `key`, return it
+                return larger_element;
+            } else {
+                // otherwise, `node` is the ceiling of `key`
+                return node;
             }
         }
     }
